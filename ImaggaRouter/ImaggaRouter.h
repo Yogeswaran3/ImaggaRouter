85922db5fd4e2e08ee66b425b06f73e2fff68a21
Yogeswaran Siva <yoges@hatchedlabs.com>
2021-11-15 18:33:34 -0500

    Initial Commit

---
+++ ImaggaRouter/ImaggaRouter.h
@@ -0,0 +1,18 @@
+//
+//  ImaggaRouter.h
+//  ImaggaRouter
+//
+//  Created by Yogeswaran Siva on 11/15/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ImaggaRouter.
+FOUNDATION_EXPORT double ImaggaRouterVersionNumber;
+
+//! Project version string for ImaggaRouter.
+FOUNDATION_EXPORT const unsigned char ImaggaRouterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImaggaRouter/PublicHeader.h>
+
+
